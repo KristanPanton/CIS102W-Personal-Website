4bf3977687d571bd61fe27daaf305014eeccfa19
KristanPanton <43457073+KristanPanton@users.noreply.github.com>
2023-11-04 11:52:23 -0400

    update logo images to svg. add project to project page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.nav-menu-icon').click(function() {
         $('.nav-link-container').toggleClass('nav-menu-vert');
+        $('.nav-menu-icon').toggleClass('menu-active');
     });
 });
